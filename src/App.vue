7597de7e8267c705e44a7e4c51875f2fb6c11940
wonderfulshan <1003580216@qq.com>
2017-12-02 15:18:01 +0800

    todolist

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:key="item.key" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
+        {{item.label}}
+      </li>
+    </ul>
+    <p>child tells me: {{ childWords }}</p>
+    <component-a msgfromfather='you die!' v-on:child-tell-me-something='listenToMyBoy'></component-a>
+  </div>
+</template>
+
+<script>
+import Store from './store'
+import ComponentA from './components/componentA'
+export default {
+  data: function () {
+    return {
+      title: 'this is a todo list',
+      items: Store.fetch(),
+      newItem: '',
+      childWords:''
+    }
+  },
+  components: { ComponentA },
+  watch: {
+    items: {
+      handler: function (items) {
+        Store.save(items)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggleFinish: function (item) {
+      item.isFinished = !item.isFinished
+    },
+    addNew: function () {
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ''
+    },
+    listenToMyBoy: function (msg) {
+      //console.log(msg);
+      this.childWords = msg;
+    }
+  }
+}
+</script>
+
+<style>
+.finished {
+  text-decoration: underline;
+}
+html {
+  height: 100%;
+}
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+#app {
+  color: #2C3E50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+#app a{
+  color: #42B983;
+  text-decoration: none;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+
+</style>
+
---
+++ src/components/componentA.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+      <h1>{{ msg }}</h1>
+      <h1>{{ msgfromfather }}</h1>
+      <button v-on:click="onClickMe">open mouse!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+      return {
+          msg: 'hello from component a!'
+      }
+  },
+  props: ['msgfromfather'],
+  methods: {
+      onClickMe: function () {
+          console.log(this.msgfromfather)
+          this.$emit('child-tell-me-something',this.msg);
+      }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42B983;
+}
+</style>
+
+
